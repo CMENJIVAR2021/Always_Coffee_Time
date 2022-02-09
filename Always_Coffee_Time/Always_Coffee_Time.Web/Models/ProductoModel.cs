366c5d4bfd8f8166400479682a85b40291baa0de
CMENJIVAR2021 <90740082+CMENJIVAR2021@users.noreply.github.com>
2022-02-08 21:44:46 -0600

    agregamos una Lista de Productos

---
+++ Always_Coffee_Time/Always_Coffee_Time.Web/Models/ProductoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Always_Coffee_Time.Web.Models
+{
+    public class ProductoModel
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
